35afe3c7b5014941afb690cbc1b2d1eeb33754e2
fengxiaochuang <me@fengxiaochuang.com>
2018-06-01 22:23:53 +0800

    删除无用的代码

--- socketServer.js
+++ socketServer.js
@@ -8,11 +8,6 @@ const http = require('http');
 const RequestInfo = require("./util/requestInfo");
 
 /**
- * Get port from environment and store in Express.
- */
-let port = normalizePort(process.env.PORT || '3000');
-
-/**
  * Create HTTP server.
  */
     // returns content-type = text/plain
@@ -26,9 +21,13 @@ const server = http.createServer((req, res) => {
  * Listen on provided port, on all network interfaces.
  */
 
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
+server.listen(3000);
+server.on('error', (err) => {
+    console.error(err);
+});
+server.on('listening', function () {
+    console.log("server is running on 127.0.0.1:3000");
+});
 // server.on("connect", function (test) {
 //     console.log("1" + test);
 // });
@@ -91,63 +90,3 @@ server.on("request", function (req) {
     // 请求地址
     console.log(req.url);
 });
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-    const port = parseInt(val, 10);
-
-    if (isNaN(port)) {
-        // named pipe
-        return val;
-    }
-
-    if (port >= 0) {
-        // port number
-        return port;
-    }
-
-    return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
-
-    const bind = typeof port === 'string'
-        ? 'Pipe ' + port
-        : 'Port ' + port;
-
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-    let addr = server.address();
-    const bind = typeof addr === 'string'
-        ? 'pipe ' + addr
-        : 'port ' + addr.port;
-    console.log('Listening on ' + bind);
-}
